7efb97c0b3c6b25d85be639722081a7c98d03bd7
Trevor Davis <tzendou@gmail.com>
2018-03-08 12:28:59 -0800

    Refactor UserTest class
    Moves duplicate code into instance variables and @BeforeEach

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -6,22 +6,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
-    Board board;
+    private Board board;
     private User user;
     private User dummy;
+    private Question question;
+    private Answer answer;
 
     @BeforeEach
     void setUp() {
         board = new Board("Java");
         user = new User(board, "User");
         dummy = new User(board, "Dummy");
+        question = new Question(user, "question");
+        board.addQuestion(question);
+        answer = new Answer(question, dummy, "answer");
+        board.addAnswer(answer);
     }
 
     @Test
     void upvotingQuestionGetsProperReputation() {
-        Question question = new Question(user, "question");
-        board.addQuestion(question);
-
         dummy.upVote(question);
 
         assertEquals(5, user.getReputation());
@@ -29,11 +32,6 @@ class UserTest {
 
     @Test
     void upvotingAnswerGetsProperReputation() {
-        Question question = new Question(user, "question");
-        Answer answer = new Answer(question, dummy, "answer");
-        board.addQuestion(question);
-        board.addAnswer(answer);
-
         user.upVote(answer);
 
         assertEquals(10, dummy.getReputation());
@@ -41,11 +39,6 @@ class UserTest {
 
     @Test
     void accceptingAnswerGetsProperReputation() {
-        Question question = new Question(user, "question");
-        Answer answer = new Answer(question, dummy, "answer");
-        board.addQuestion(question);
-        board.addAnswer(answer);
-
         user.acceptAnswer(answer);
 
         assertEquals(15, dummy.getReputation());
